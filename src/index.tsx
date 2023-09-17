ec2b47b25416d55beba556db0aead2a994ce3e5b
Josh Marantz <jmarantz@thelabnyc.com>
2023-09-17 19:32:05 -0400

    I no longer hate this code

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,14 @@ import { isObject, mergeDeep, takeUntil } from "./utils";
 
 function RenderBlockComponent({
 	block,
-	config: blockConfig,
+	config,
 	children,
 }: React.PropsWithChildren<{
 	block: RawDraftContentBlock;
-	config: BlockComponent;
+	config: RTConfig;
 }>) {
+	const blockConfig =
+		config.blockComponents[block.type] ?? config.defaultBlockComponent;
 	const Component =
 		isObject(blockConfig) && "element" in blockConfig
 			? blockConfig.element
@@ -81,60 +83,6 @@ function renderStyledText(
 	return result;
 }
 
-function NestedBlocks({
-	group = [] as RawDraftContentBlock[],
-	config = defaultConfig,
-}) {
-	group = [...group];
-	const { type, depth: rootDepth } = group[0];
-	const rootConfig =
-		config.blockComponents[type] ?? config.defaultBlockComponent;
-	const WrapperComponent =
-		isObject(rootConfig) && "wrapper" in rootConfig
-			? rootConfig.wrapper
-			: React.Fragment;
-	let children = [] as React.ReactNode[];
-
-	while (group.length) {
-		const subgroup = takeUntil((x) => x.depth > rootDepth, group);
-		let nestingParent: RawDraftContentBlock | undefined = undefined;
-		if (group.length) {
-			// If there are elements with greater depth, they're nested under the last element of the subgroup
-			nestingParent = subgroup.pop();
-		}
-		children = children.concat(
-			subgroup.map((block) => (
-				<RenderBlockComponent
-					key={block.key}
-					block={block}
-					config={rootConfig}
-				/>
-			)),
-		);
-
-		if (nestingParent) {
-			// nest everything until we get back to the rootDepth
-			const subgroup = takeUntil((x) => x.depth === rootDepth, group);
-
-			children.push(
-				<RenderBlockComponent
-					key={nestingParent.key}
-					block={nestingParent}
-					config={rootConfig}
-				>
-					<NestedBlocks group={subgroup} config={config} />
-				</RenderBlockComponent>,
-			);
-		}
-	}
-
-	if (typeof WrapperComponent === "string") {
-		return <WrapperComponent>{children}</WrapperComponent>;
-	} else {
-		return <WrapperComponent depth={rootDepth}>{children}</WrapperComponent>;
-	}
-}
-
 export function RichText({
 	config: propsConfig,
 	extendConfig,
@@ -151,101 +99,68 @@ export function RichText({
 		config = mergeDeep(defaultConfig, extendConfig ?? {});
 	}
 
-	return groupBlocks(json.blocks).flatMap((blocksOfType) => {
-		if (blocksOfType.some((x) => x.depth > 0))
-			return <NestedBlocks group={blocksOfType} />;
-
-		const blockConfig =
-			config.blockComponents[blocksOfType[0].type] ??
-			config.defaultBlockComponent;
-		if (typeof blockConfig === "string") {
-			const BlockComponent = blockConfig;
-			return blocksOfType.map((block) => {
-				const children = renderStyledText(
-					block.text,
-					0,
-					block.inlineStyleRanges,
-				);
-				return <BlockComponent key={block.key}>{children}</BlockComponent>;
-			});
-		} else if ("wrapper" in blockConfig) {
-			const WrapperComponent = blockConfig.wrapper;
-			const BlockComponent = blockConfig.element;
-			return (
-				<WrapperComponent depth={0} key={blocksOfType[0].key}>
-					{blocksOfType.map((block) => {
-						const children = renderStyledText(
-							block.text,
-							0,
-							block.inlineStyleRanges,
-						);
-						if (typeof BlockComponent === "string")
-							return (
-								<BlockComponent key={block.key}>{children}</BlockComponent>
-							);
-						else
-							return (
-								<BlockComponent key={block.key} block={block}>
-									{children}
-								</BlockComponent>
-							);
-					})}
-				</WrapperComponent>
-			);
-		} else {
-			const BlockComponent = blockConfig;
-			return blocksOfType.map((block) => {
-				const children = renderStyledText(
-					block.text,
-					0,
-					block.inlineStyleRanges,
-				);
-				return (
-					<BlockComponent key={block.key} block={block}>
-						{children}
-					</BlockComponent>
-				);
-			});
-		}
-	});
-
-	function renderStyledText(
-		text: string,
-		offset: number,
-		[activeRange, ...ranges]: RawDraftInlineStyleRange[],
-	) {
-		const result: Array<React.ReactNode> = [];
-		while (text.length && activeRange) {
-			// If there is unstyled text before the range starts, chop it off
-			if (offset < activeRange.offset) {
-				const takeUntil = activeRange.offset - offset;
-				const t = text.slice(0, takeUntil);
-				result.push(<React.Fragment key={t + text.length}>{t}</React.Fragment>);
-				text = text.slice(takeUntil);
-				offset = activeRange.offset;
-			}
-			if (!text) return result;
-
-			// render the text in the range and any additional styles that should be wrapped around it
-			const length = activeRange.length ?? 1; // y no length if length is 1, that's not what types say :(
-			const styledText = renderStyledText(
-				text.slice(0, length),
-				offset,
-				ranges,
-			);
-			const InlineComponent =
-				config.inlineStyleComponents[activeRange.style] ??
-				config.defaultInlineStyleComponent;
-			result.push(
-				<InlineComponent key={text.length}>{styledText}</InlineComponent>,
-			);
-			offset += length;
-			text = text.slice(length);
-			[activeRange, ...ranges] = ranges;
-		}
-		if (text) result.push(<React.Fragment key={text}>{text}</React.Fragment>);
-		return result;
+	const blocks = [...json.blocks];
+	const result: React.ReactNode[] = [];
+	while (blocks.length) {
+		const matchingBlock = blocks[0];
+		// grab all blocks like this or anything nested under blocks like this
+		const blocksOfType = takeUntil(
+			(x) => x.type !== matchingBlock.type || x.depth < matchingBlock.depth,
+			blocks,
+		);
+		result.push(
+			<BlockGroup
+				//Other react elements will use this key but not in this array
+				key={matchingBlock.key}
+				blocks={blocksOfType}
+				config={config}
+			/>,
+		);
 	}
+	return result;
+}
+
+function BlockGroup({
+	blocks,
+	config,
+}: {
+	blocks: RawDraftContentBlock[];
+	config: RTConfig;
+}) {
+	blocks = [...blocks];
+	const groupDepth = blocks[0].depth;
+	const groupConfig =
+		config.blockComponents[blocks[0].type] ?? config.defaultBlockComponent;
+	const GroupWrapper =
+		isObject(groupConfig) && "wrapper" in groupConfig
+			? groupConfig.wrapper
+			: React.Fragment;
+	const result: React.ReactNode[] = [];
+	while (blocks.length) {
+		const currentBlock = blocks.shift();
+		if (!currentBlock) throw new Error("this should be impossible :)");
+		let children: React.ReactNode = null;
+		if (blocks[0]?.depth > currentBlock.depth) {
+			const nestedBlocks = takeUntil(
+				(x) => x.depth <= currentBlock.depth,
+				blocks,
+			);
+			children = <BlockGroup blocks={nestedBlocks} config={config} />;
+		}
+		result.push(
+			<RenderBlockComponent
+				key={currentBlock.key}
+				block={currentBlock}
+				config={config}
+			>
+				{children}
+			</RenderBlockComponent>,
+		);
+	}
+	if (typeof GroupWrapper === "string" || GroupWrapper === React.Fragment)
+		// @ts-expect-error
+		return <GroupWrapper>{result}</GroupWrapper>;
+	else return <GroupWrapper depth={groupDepth}>{result}</GroupWrapper>;
 }
 
 type RenderedBlockComponent =
@@ -321,16 +236,3 @@ export const defaultConfig: RTConfig = {
 	},
 	defaultInlineStyleComponent: ({ children }) => <>{children}</>,
 };
-
-function groupBlocks<T extends { type: string; depth: number }>(
-	arr: Array<T>,
-): Array<Array<T>> {
-	arr = [...arr]; // Should I write my code without mutation? I dunno
-	const result: Array<Array<T>> = [];
-	while (arr.length) {
-		// Type is always a string because the array isn't empty
-		const type = arr[0].type;
-		result.push(takeUntil((x) => x.type !== type && x.depth === 0, arr));
-	}
-	return result;
-}
